6f265ba9b97651424bc153eecc59606735f3c31d
tokamaky <jxh186045@gmail.com>
2024-12-18 17:31:27 -0500

    backend update: debug SDK

--- xiaohangapi-backend/xiaohangapi-interface/src/main/java/com/xiaohang/xiaohangapiinterface/controller/NameController.java
+++ xiaohangapi-backend/xiaohangapi-interface/src/main/java/com/xiaohang/xiaohangapiinterface/controller/NameController.java
@@ -1,7 +1,10 @@
 package com.xiaohang.xiaohangapiinterface.controller;
 
 import com.xiaohang.xiaohangapiclientsdk.model.User;
+import com.xiaohang.xiaohangapiclientsdk.utils.SignUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Name API
@@ -23,28 +26,28 @@ public class NameController {
     }
 
     @PostMapping("/user")
-    public String getUsernameByPost(@RequestBody User user) {
-//        String accessKey = request.getHeader("accessKey");
-//        String nonce = request.getHeader("nonce");
-//        String timestamp = request.getHeader("timestamp");
-//        String sign = request.getHeader("sign");
-//        String body = request.getHeader("body");
-//        // todo 实际情况应该是去数据库中查是否已分配给用户
-//        if (!accessKey.equals("yupi")) {
-//            throw new RuntimeException("无权限");
-//        }
-//        if (Long.parseLong(nonce) > 10000) {
-//            throw new RuntimeException("无权限");
-//        }
+    public String getUsernameByPost(@RequestBody User user, HttpServletRequest request) {
+        String accessKey = request.getHeader("accessKey");
+        String nonce = request.getHeader("nonce");
+        String timestamp = request.getHeader("timestamp");
+        String sign = request.getHeader("sign");
+        String body = request.getHeader("body");
+        // todo 实际情况应该是去数据库中查是否已分配给用户
+        if (!accessKey.equals("xiaohang")) {
+            throw new RuntimeException("无权限");
+        }
+        if (Long.parseLong(nonce) > 10000) {
+            throw new RuntimeException("无权限");
+        }
         // todo 时间和当前时间不能超过 5 分钟
-//        if (timestamp) {
+      //  if (timestamp) {
 //
-//        }
+       //  }
         // todo 实际情况中是从数据库中查出 secretKey
-//        String serverSign = SignUtils.genSign(body, "abcdefgh");
-//        if (!sign.equals(serverSign)) {
-//            throw new RuntimeException("无权限");
-//        }
+        String serverSign = SignUtils.genSign(body, "abcdefgh");
+        if (!sign.equals(serverSign)) {
+          throw new RuntimeException("无权限");
+        }
         // todo 调用次数 + 1 invokeCount
         String result = "POST 用户名字是" + user.getUsername();
         return result;
