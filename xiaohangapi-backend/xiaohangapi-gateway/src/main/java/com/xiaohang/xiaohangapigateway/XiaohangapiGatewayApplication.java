57442d8989c2022e1e8e975c7632c262615b6ac4
tokamaky <jxh186045@gmail.com>
2024-12-22 00:00:28 -0500

    backend create: generate gateway

---
+++ xiaohangapi-backend/xiaohangapi-gateway/src/main/java/com/xiaohang/xiaohangapigateway/XiaohangapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohang.xiaohangapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohangapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohangapiGatewayApplication.class, args);
+    }
+
+}
---
+++ xiaohangapi-backend/xiaohangapi-gateway/src/test/java/com/xiaohang/xiaohangapigateway/XiaohangapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohang.xiaohangapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohangapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
