a73920298f7835efab19402c33b8be7d6e490a6a
tokamaky <jxh186045@gmail.com>
2026-04-26 23:42:07 -0400

    update url

--- xiaohangapi-backend/xiaohangapi-gateway/src/main/java/com/xiaohang/xiaohangapigateway/CustomGlobalFilter.java
+++ xiaohangapi-backend/xiaohangapi-gateway/src/main/java/com/xiaohang/xiaohangapigateway/CustomGlobalFilter.java
@@ -45,13 +45,13 @@ import java.util.Objects;
 @Component
 public class CustomGlobalFilter implements GlobalFilter, Ordered {
 
-    @DubboReference(protocol = "tri", check = false, url = "${dubbo.provider.url:tri://backend-production-796b.up.railway.app:20880}")
+    @DubboReference(protocol = "tri", check = false, url = "${dubbo.provider.url:https://backend-production-796b.up.railway.app}")
     private InnerUserService innerUserService;
 
-    @DubboReference(protocol = "tri", check = false, url = "${dubbo.provider.url:tri://backend-production-796b.up.railway.app:20880}")
+    @DubboReference(protocol = "tri", check = false, url = "${dubbo.provider.url:https://backend-production-796b.up.railway.app}")
     private InnerInterfaceInfoService innerInterfaceInfoService;
 
-    @DubboReference(protocol = "tri", check = false, url = "${dubbo.provider.url:tri://backend-production-796b.up.railway.app:20880}")
+    @DubboReference(protocol = "tri", check = false, url = "${dubbo.provider.url:https://backend-production-796b.up.railway.app}")
     private InnerUserInterfaceInfoService innerUserInterfaceInfoService;
 
     private static final List<String> IP_WHITE_LIST = Arrays.asList("127.0.0.1", "127.0.0.2", "0:0:0:0:0:0:0:1");
